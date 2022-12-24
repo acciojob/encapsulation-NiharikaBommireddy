8ce40d006fa95d8fd77a83ad6de0a3d74346d486
Niharika Bommireddy <niharikabommireddy21@gmail.com>
2022-12-24 19:18:48 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args)
+  {
+    RWOnly rw = new RWOnly();
+    // 'name' has private access in 'com.driver.RWOnly'
+    //rw.name = "Niharika";
+    System.out.println("Error: 'name' has private access in 'com.driver.RWOnly'");
+    rw.setName("Niharika");
+    System.out.println(rw.getName());
+
+  }
 }
